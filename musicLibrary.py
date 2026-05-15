6c84f0afb6229365ef84a95d639b49653d356ca4
Amritanshu kumar <Amritanshukumar2001@gmail.com>
2026-05-15 11:02:59 +0530

    jarvis

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "tu har lamha" : "https://youtu.be/1L2JJz0Kr-I?si=oB9ugj0QqDMVVyFE",
+    "barbaad":"https://youtu.be/W1y8blwMLxY?si=6yDXCCpizcQAb_14",
+    "bairan":"https://youtu.be/oafxkMv4xnc?si=GF_rmWU8acS8SsV_"
+}
